0ab962efcb5483f74da9e0896b00ba402455a4e1
naj1024 <naj1024@machine>
2026-05-17 12:19:35 +0100

    changed average constant fpr effective sr

--- src/misc/TimesAndAverages.py
+++ src/misc/TimesAndAverages.py
@@ -9,7 +9,7 @@ class TimesAndAverages:
     def __init__(self):
         self.loop_time = Ewma.Ewma(0.001)
         self.cpu_core = Ewma.Ewma(0.01)
-        self.effective_sps = Ewma.Ewma(0.1)
+        self.effective_sps = Ewma.Ewma(0.01)
 
         self.source_stats_time = 0  # when we will send our config to the UI
         self.fps_update_time = 0
